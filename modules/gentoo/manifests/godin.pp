3c18803fda318d7af260decb7753aead7da898e9
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-06 23:22:20 +0000

    added svn dep

--- modules/gentoo/manifests/godin.pp
+++ modules/gentoo/manifests/godin.pp
@@ -1,5 +1,11 @@
 
 class gentoo::godin {
+	package {
+		# needed for syncing godwin overlay
+		# will move this incantation to a nicer location when i configure svn
+		"subversion":
+			ensure => installed,
+	}
 	exec {
 		"add godin repo to layman":
 			command => "/usr/bin/layman -a godin",
