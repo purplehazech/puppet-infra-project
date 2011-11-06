71861a497762719aa6a9596fe3b11a6b18d29851
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-06 21:59:56 +0000

    initial repo integration bootstrapper and config
    based on the scaling puppet with git article and tested on gentoo

---
+++ bootstrap.pp
@@ -0,0 +1,52 @@
+
+user {
+	"git":
+		ensure => present,
+		home   => "/var/git"
+}
+
+file {
+	"/var/git":
+		ensure  => directory,
+		owner   => git,
+		require => User['git']
+	;
+	"/var/git/puppet/":
+		ensure  => directory,
+		owner   => git,
+		require => File['/var/git']
+}
+
+package {
+	"git":
+		ensure => installed
+}
+
+exec {
+	"bare puppet git repo":
+		cwd     => "/var/git/puppet",
+		user    => git,
+		command => "/usr/bin/git init --bare",
+		creates => "/var/git/puppet/HEAD",
+		require => [
+			Package['git'],
+			File['/var/git/puppet']
+		]
+	;
+	"remote puppet git repo":
+		cwd     => "/var/git/puppet",
+		user    => git,
+		command => "/usr/bin/git remote add origin git://github.com/purplehazech/puppet-infra-project",
+		onlyif  => "/bin/grep -v origin /var/git/puppet/config && exit 1",
+		require => Exec["bare puppet git repo"]
+}
+file {
+	"/var/git/puppet/hooks/post-receive":
+		ensure  => file,
+		owner   => git,
+		mode    => 555,
+		content => "git archive --format=tar HEAR | (cd /etc/puppet/ && tar -xf -)",
+		require => Exec["bare puppet git repo"]
+}
+
+
