1b2d5eee902013dd4307d52fe35672bb54a4be78
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-07 18:51:58 +0000

    cli tool install
    i need the cli tool for jenkins to be setuppable through puppet

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -3,7 +3,22 @@ class jenkins {
 
 	package {
 		"jenkins-bin":
-			ensure => installed,
+			ensure  => installed,
 	}
 
+	file {
+
+		"/usr/bin/jenkins-cli":
+			ensure => file,
+			content => "java -jar /usr/lib/jenkins/jenkins-cli.jar $0",
+			mode => 555
+	}
+	exec {
+		"/usr/lib/jenkins/jenkins-cli.jar":
+			cwd     => "/tmp",
+			command => "/usr/bin/jar -xvf /usr/lib/jenkins/jenkins.war WEB-INF/jenkins-cli.jar && mv WEB-INF/jenkins-cli.jar /usr/lib/jenkins/",
+			creates => "/usr/lib/jenkins/jenkins-cli.jar"
+	}
 }
+
+import "*.pp"
