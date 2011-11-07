9b43daa39b4d51158c49e586a8bd8a51688a0658
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-07 19:51:41 +0000

    added creates

--- modules/jenkins/manifests/github.pp
+++ modules/jenkins/manifests/github.pp
@@ -2,6 +2,7 @@
 class jenkins::github {
 	exec {
 		"install github plugin":
-			command => "/usr/bin/jenkins-cli -s http://localhost:8080 install-plugin github"
+			command => "/usr/bin/jenkins-cli -s http://localhost:8080 install-plugin github",
+			creates => "/var/lib/jenkins/home/plugins/github.hpi"
 	}
 }
