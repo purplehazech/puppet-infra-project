fa647a47adf5aa9f60eeb35cbc1d4f185bd9ad99
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-12 05:07:42 +0100

    tab fix, adding service to jenkins config and some @todos

--- modules/jenkins/manifests/init.pp
+++ modules/jenkins/manifests/init.pp
@@ -1,24 +1,30 @@
 
 class jenkins {
 
-       package {
-           "jenkins-bin":
-               ensure  => installed,
-               require => Package['ant']
-       }
+    package {
+        "jenkins-bin":
+            ensure  => installed,
+            require => Package['ant']
+    }
 
-       file {
-           "/usr/bin/jenkins-cli":
-               ensure  => file,
-               content => "java -jar /usr/lib/jenkins/jenkins-cli.jar \$@",
-               mode    => 555
-       }
-       exec {
-           "/usr/lib/jenkins/jenkins-cli.jar":
-               cwd     => "/tmp",
-               command => "/usr/bin/jar -xvf /usr/lib/jenkins/jenkins.war WEB-INF/jenkins-cli.jar && mv WEB-INF/jenkins-cli.jar /usr/lib/jenkins/",
-               creates => "/usr/lib/jenkins/jenkins-cli.jar"
-       }
+    file {
+        "/usr/bin/jenkins-cli":
+            ensure  => file,
+            content => "java -jar /usr/lib/jenkins/jenkins-cli.jar \$@",
+            mode    => 555
+    }
+    exec {
+        "/usr/lib/jenkins/jenkins-cli.jar":
+            cwd     => "/tmp",
+            command => "/usr/bin/jar -xvf /usr/lib/jenkins/jenkins.war WEB-INF/jenkins-cli.jar && mv WEB-INF/jenkins-cli.jar /usr/lib/jenkins/",
+            creates => "/usr/lib/jenkins/jenkins-cli.jar"
+    }
+    # @todo chmod -R g+w /etc/puppet/ /var/git/puppet/ && chgrp -R /etc/puppet/ /var/git/puppet
+    service {
+        "jenkins":
+            ensure => started
+    }
+       
 }
 
 import "*.pp"
