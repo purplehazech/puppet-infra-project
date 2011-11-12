66fe581b555148072329a869000c41f04200256e
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-12 13:03:00 +0100

    fix ownership on sodoers file

--- modules/sudo/manifests/jenkins.pp
+++ modules/sudo/manifests/jenkins.pp
@@ -4,13 +4,15 @@ class sudo::jenkins {
     file {
         "/etc/sudoers.d/jenkins_puppet":
             ensure  => file,
+            owner   => root,
+            mode    => 440,
             content => template("sudo/jenkins_puppet_sudoers.erb");
 
         "/usr/sbin/jenkins-recieve":
             ensure  => file,
-            content => "/usr/bin/git archive --format=tar HEAD | (cd /etc/puppet/ && tar xf - )",
             owner   => root,
-            mode    => 500
+            mode    => 500,
+            content => "/usr/bin/git archive --format=tar HEAD | (cd /etc/puppet/ && tar xf - )";
     }
 }
 
