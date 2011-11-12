63c2050c71f835d260509f88dc1f173085103519
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-12 12:52:26 +0100

    fixed
    ensure exists is what clashed with content

--- modules/sudo/manifests/jenkins.pp
+++ modules/sudo/manifests/jenkins.pp
@@ -3,12 +3,11 @@ class sudo::jenkins {
 
     file {
         "/etc/sudoers.d/jenkins_puppet":
-            ensure  => exists,
-            content => template("sudo/jenkins_puppet_sudoers.erb")
-    }
-    file {
+            ensure  => file,
+            content => template("sudo/jenkins_puppet_sudoers.erb");
+
         "/usr/sbin/jenkins-recieve":
-            ensure  => exists,
+            ensure  => file,
             content => "/usr/bin/git archive --format=tar HEAD | (cd /etc/puppet/ && tar xf - )",
             owner   => root,
             mode    => 500
