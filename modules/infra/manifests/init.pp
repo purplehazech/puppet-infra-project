117df231fb5fc31961b3c95f254e97ed6cb88da6
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-25 23:41:14 +0100

    code cleanup, moving methods where they belong

--- modules/infra/manifests/init.pp
+++ modules/infra/manifests/init.pp
@@ -5,7 +5,20 @@ class infra {
     file {
         # do this in jenkins after bootstrap!
         "/var/git/puppet/hooks/post-receive":
-            ensure  => empty,
+            ensure  => empty;
+
+        # these scripts get registered with sudo in sudo::jenkins
+        "/usr/sbin/jenkins-receive":
+            ensure  => file,
+            owner   => root,
+            mode    => 500,
+            content => "cd /var/git/puppet && /usr/bin/git archive --format=tar HEAD | (cd /etc/puppet/ && tar xf - )";
+
+        "/usr/sbin/puppet-apply":
+            ensure  => file,
+            owner   => root,
+            mode    => 500,
+            content => "/usr/bin/puppet apply --modulepath=/etc/puppet/modules/ /etc/puppet/site.pp -v --color false"
     }
 
 }
--- modules/sudo/manifests/jenkins.pp
+++ modules/sudo/manifests/jenkins.pp
@@ -6,19 +6,7 @@ class sudo::jenkins {
             ensure  => file,
             owner   => root,
             mode    => 440,
-            content => template("sudo/jenkins_puppet_sudoers.erb");
-
-        "/usr/sbin/jenkins-receive":
-            ensure  => file,
-            owner   => root,
-            mode    => 500,
-            content => "cd /var/git/puppet && /usr/bin/git archive --format=tar HEAD | (cd /etc/puppet/ && tar xf - )";
-
-        "/usr/sbin/puppet-apply":
-            ensure  => file,
-            owner   => root,
-            mode    => 500,
-            content => "/usr/bin/puppet apply --modulepath=/etc/puppet/modules/ /etc/puppet/site.pp -v --color false"
+            content => template("sudo/jenkins_puppet_sudoers.erb")
     }
 }
 
