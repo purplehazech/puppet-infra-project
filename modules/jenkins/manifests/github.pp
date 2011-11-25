66a09848b9984eb56b58038c0f2c96e1121664d3
Lucas Bickel <hairmare@purplehaze.ch>
2011-11-26 00:28:54 +0100

    refactoring to new jenkins::plugin define installation

--- modules/jenkins/manifests/github.pp
+++ modules/jenkins/manifests/github.pp
@@ -1,9 +1,7 @@
 
 class jenkins::github {
-	exec {
-		"install github plugin":
-			command => "/usr/bin/jenkins-cli -s http://$ipaddress:8080 install-plugin github",
-			creates => "/var/lib/jenkins/home/plugins/github.hpi",
-            require => File["/usr/bin/jenkins-cli"]
-	}
+    jenkins::plugin {
+        "github":
+            ensure => present
+    }
 }
--- modules/jenkins/manifests/htmlpublisher.pp
+++ modules/jenkins/manifests/htmlpublisher.pp
@@ -1,9 +1,7 @@
 
 class jenkins::htmlpublisher {
-    exec {
-        "install htmlpublisher plugin":
-            command => "/usr/bin/jenkins-cli -s http://$ipaddress:8080 install-plugin htmlpublisher",
-			creates => "/var/lib/jenkins/home/plugins/htmlpublisher.hpi",
-            require => File["/usr/bin/jenkins-cli"]
+    jenkins::plugin {
+        "htmlpublisher":
+            ensure => present
     }
 }
