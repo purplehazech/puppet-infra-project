341191e99cf1d51dfd35744825665c642d100913
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-24 00:25:55 +0200

    the things i planned on checking in

--- modules/gentoo/manifests/rabe.pp
+++ modules/gentoo/manifests/rabe.pp
@@ -3,11 +3,11 @@ class gentoo::rabe {
 	line { 'layman-overlays':
 		file => '/etc/make.conf',
 		line => 'source /var/lib/layman/make.conf',
-	},
+	}
 	exec {
 		"add rabe repo to layman":
 			command => "/usr/bin/layman -a rabe",
 			creates => "/var/lib/layman/rabe/",
-			require => Line['sync layman repos']
+			require => Exec['sync layman repos']
 	}
 }
