52a4622cedf4be8c6f9193beef4788d596dc5a4f
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-25 18:38:15 +0200

    missing file from last commit

--- modules/infra/manifests/gentoo.pp
+++ modules/infra/manifests/gentoo.pp
@@ -1,24 +1,7 @@
 
 class infra::gentoo {
 
+    include layman
     include infra
 
-	package {
-		"layman":
-			ensure => installed
-	}
-
-	file {
-		"/etc/layman/layman.cfg":
-			ensure  => file,
-			content => template("infra/gentoo_layman.cfg"),
-			require => Package["layman"]
-	}
-
-	exec {
-		"sync layman repos":
-			command => "/usr/bin/layman -L",
-			onlyif  => "/bin/ls -al /var/lib/layman/cache*xml && exit 1 || exit 0",
-			require => File["/etc/layman/layman.cfg"]
-	}
 }
