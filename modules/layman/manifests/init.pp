960656ae93944d652b58c6ce6c66da53224f691e
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-25 18:23:58 +0200

    some refactoring of layman config
    i need this to be way more flexible than it is now and i haven't
    found any smart enough implementations on the interwebs.

---
+++ modules/layman/manifests/init.pp
@@ -0,0 +1,24 @@
+
+class layman {
+
+	package {
+		"layman":
+			ensure => installed
+	}
+
+	file {
+		"/etc/layman/layman.cfg":
+			ensure  => file,
+			content => template("layman/gentoo_layman.cfg"),
+			require => Package["layman"]
+	}
+
+	exec {
+		"sync layman repos":
+			command => "/usr/bin/layman -L",
+			onlyif  => "/bin/ls -al /var/lib/layman/cache*xml && exit 1 || exit 0",
+			require => File["/etc/layman/layman.cfg"]
+	}
+	
+}
+
