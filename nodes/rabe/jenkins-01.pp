bc46267a66b31fb28166bbe3a6d207d92cce2125
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-25 19:55:22 +0200

    more rewriting to external puppet module
    just realized that i might have to port the plugin installation
    routine from my setup to the plugin (although, mime will never
    work in enterprise setups due to letting jenkins update himself)

--- nodes/rabe/jenkins-01.pp
+++ nodes/rabe/jenkins-01.pp
@@ -1,14 +1,21 @@
 
 node "jenkins-01.hq.rabe.ch" {
+
     include infra::gentoo
+
     include gentoo::rabe # our own overlay
     include gentoo::godin # thats where jenkins is from
 
-    class jenkins { }
     include ant
-    include jenkins::github
     include sudo
-    include jenkins::puppet
-    include jenkins::mantis
-    include jenkins::php
+
+    class jenkins { }
+    jenkins::plugin {
+      analysis-core: ;
+      analysis-collector: ;
+      github: ;
+      puppet: ;
+      mantis: ;
+      php: ;
+    }
 }
