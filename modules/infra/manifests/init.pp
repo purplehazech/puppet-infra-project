144f821b93f5e4c6ddbc933e47906bcc067912ab
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-28 19:00:34 +0200

    install zabbix agent on all machines
    i plan on having zabbix as a baseline requirement for all machines
    that are under infra control

--- modules/infra/manifests/init.pp
+++ modules/infra/manifests/init.pp
@@ -2,4 +2,20 @@ import "*.pp"
 
 class infra {
 
+    package { "zabbix":
+        ensure => installed
+    }
+
+    file { '/etc/zabbix/zabbix_agentd.conf':
+        contents => template("zabbix/agentd.conf")
+    }
+
+    service { "zabbix-agentd":
+        ensure => running,
+	require => [
+            Package['zabbix'],
+	    File['/etc/zabbix/zabbix_agentd.conf']
+	]
+    }
+
 }
