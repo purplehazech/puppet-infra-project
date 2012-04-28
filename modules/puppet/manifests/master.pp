d897e4bafe0b42fa9589d4734f5961903bed769a
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-28 19:00:05 +0200

    rather hacky dashboard setup

--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -6,8 +6,26 @@ class puppet::master {
 	"puppet-infra-project": 
 	    ensure => latest;
 	"puppet-dashboard":
-	    ensure => installed
+	    # latest+noop to make sure i don't forget to cleanup when i upgrade
+	    ensure => latest,
+	    noop => true
     }
+
+    # @todo dashboard needs to stabilize, i hacked package.keywords by hand for now
+    # use flags are here though
+    file {
+    	"/etc/portage/package.use/puppet-dashboard":
+		content => "app-admin/puppet-dashboard mysql imagemagick fastcgi\ndev-ruby/activerecord mysql\ndev-lang/ruby threads"
+	"/etc/portage/package.keywords/puppet-dashboard": ;
+    }
+    # also i hacked the init script to also start delayed_jobs
+    # then there where the whole changes to puppet-dashboards
+    # config that live in /var/lib/puppet-dashboard/config
+    # and should also get ported in here.
+    # basically i'll try and revist this part of the puppet
+    # scripts when the dashboard is a bit more mature.
+    # i might even decide to just pop the report data into
+    # zabbix without using the dashboard...
 
     service { "puppetmaster":
         ensure => running,
