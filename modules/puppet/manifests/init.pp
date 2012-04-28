cb5a6db5c314f81c6365cd32c695ff917c756080
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-28 23:26:36 +0200

    dox

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -20,6 +20,14 @@ class puppet {
 	]
     }
 
+    # trigger eix-update when it is marked as stale by the file
+    # /var/lib/puppet/state/eix.stale. This is meant to be used
+    # as an external trigger source. I shall add created this 
+    # file after building new versions of the infra on jenkins
+    # to puppetmaster since i sont want to wait for cron on 
+    # that machines. i'll wait for this till i have an esb and
+    # do such stuff by hand for now.
+
     exec { 
         "rm-eix-stalefile":
             command => "/bin/rm -f /var/lib/puppet/state/eix.stale",
