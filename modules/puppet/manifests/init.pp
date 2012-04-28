0ab1434c62f07184e553fa25bf50beb23302f16c
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-28 23:24:03 +0200

    bugfix

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -22,10 +22,11 @@ class puppet {
 
     exec { 
         "rm-eix-stalefile":
-            command => "/bin/rm -f /var/lib/puppet/state/eix.stale";
+            command => "/bin/rm -f /var/lib/puppet/state/eix.stale",
+	    onlyif => "/usr/bin/test -f /var/lib/puppet/state/eix.stale";
 
         "/usr/bin/eix-update":
-	    unless => "/usr/bin/test ! -f /var/lib/puppet/state/eix.stale"
+	    unless => "/usr/bin/test ! -f /var/lib/puppet/state/eix.stale",
             require => [
 	        Service["puppet"],
 		Exec["sync layman repos"],
