fe298e05d71b61de328a64d31ff2baf708ebf32c
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-29 12:22:59 +0200

    useflags for busmaster
    pulls in a rather complete php build

--- modules/vserver/manifests/busmaster.pp
+++ modules/vserver/manifests/busmaster.pp
@@ -1,6 +1,11 @@
 
 class vserver::busmaster {
 
+    # pulling in a not so minimal php config 
+    file { "/etc/portage/package.use/rabe-bustmaster":
+        content => template("vserver/busmaster.use.erb
+    }
+
     package { "rabe-busmaster":; }
 
 }
