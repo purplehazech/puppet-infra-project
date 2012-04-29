2b63ef31111f784dbb0f4e9a78f77697763ee2fb
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-29 13:06:24 +0200

    ensure some files for busmaster

--- modules/vserver/manifests/busmaster.pp
+++ modules/vserver/manifests/busmaster.pp
@@ -1,9 +1,14 @@
 
 class vserver::busmaster {
 
-    # pulling in a not so minimal php config 
-    file { "/etc/portage/package.use/rabe-bustmaster":
-        content => template("vserver/busmaster.use.erb")
+    file { 
+        # pulling in a not so minimal php config 
+        "/etc/portage/package.use/rabe-bustmaster":
+            content => template("vserver/busmaster.use.erb")
+	"/etc/busmaster":
+	    ensure => directory;
+	"/etc/busmaster/gearman.ini":
+	    ensure => file;
     }
 
     package { "rabe-busmaster":; }
