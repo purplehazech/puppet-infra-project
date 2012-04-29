cb946b7e16a3f0c36d280aee5a7cd614dfb273c0
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-29 13:11:42 +0200

    bugfix

--- modules/vserver/manifests/busmaster.pp
+++ modules/vserver/manifests/busmaster.pp
@@ -4,7 +4,7 @@ class vserver::busmaster {
     file { 
         # pulling in a not so minimal php config 
         "/etc/portage/package.use/rabe-bustmaster":
-            content => template("vserver/busmaster.use.erb")
+            content => template("vserver/busmaster.use.erb");
 	"/etc/busmaster":
 	    ensure => directory;
 	"/etc/busmaster/gearman.ini":
