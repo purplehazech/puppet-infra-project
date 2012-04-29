464b0878a0e15610706a3abd9ae378e770552251
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-29 12:50:03 +0200

    bugfix

--- modules/vserver/manifests/busmaster.pp
+++ modules/vserver/manifests/busmaster.pp
@@ -3,7 +3,7 @@ class vserver::busmaster {
 
     # pulling in a not so minimal php config 
     file { "/etc/portage/package.use/rabe-bustmaster":
-        content => template("vserver/busmaster.use.erb
+        content => template("vserver/busmaster.use.erb")
     }
 
     package { "rabe-busmaster":; }
