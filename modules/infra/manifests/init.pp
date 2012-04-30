0a00e89ee9db88aa5568fd3bc744b346937e39a0
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-30 19:51:48 +0200

    refactoring zabbix stuff

--- modules/infra/manifests/init.pp
+++ modules/infra/manifests/init.pp
@@ -1,21 +1,5 @@
-import "*.pp"
-
 class infra {
 
-    package { "zabbix":
-        ensure => installed
-    }
-
-    file { '/etc/zabbix/zabbix_agentd.conf':
-        content => template("zabbix/zabbix_agentd.conf.erb")
-    }
-
-    service { "zabbix-agentd":
-        ensure => running,
-	require => [
-            Package['zabbix'],
-	    File['/etc/zabbix/zabbix_agentd.conf']
-	]
-    }
+    require Class['zabbix']
 
 }
---
+++ modules/zabbix/manifests/init.pp
@@ -0,0 +1,20 @@
+class zabbix {
+
+    package { "zabbix":
+        ensure => installed
+    }
+
+    file { '/etc/zabbix/zabbix_agentd.conf':
+        content => template("zabbix/zabbix_agentd.conf.erb")
+    }
+
+    service { "zabbix-agentd":
+        ensure => running,
+        require => [
+            Package['zabbix'],
+            File['/etc/zabbix/zabbix_agentd.conf']
+        ]
+    }
+
+
+}
