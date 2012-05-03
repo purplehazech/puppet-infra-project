e6864f32f3d35aba3f3b8e90f3cefaae9d8555d4
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-03 20:57:36 +0200

    adding jenkins user to reposync

--- modules/vserver/manifests/reposync.pp
+++ modules/vserver/manifests/reposync.pp
@@ -21,4 +21,31 @@ class vserver::reposync {
         ensure => running;
     }
 
+    user { "jenkins":
+        ensure => present,
+        shell => "/bin/sh",
+        home => "/var/lib/jenkins"
+    }
+
+    file {
+        "/var/lib/jenkins":
+            ensure => directory,
+            owner => 'jenkins';
+        "/var/lib/jenkins/.ssh":
+            ensure => directory,
+            owner => 'jenkins'
+            require => File['/var/lib/jenkins'];
+        "/var/lib/jenkins/.ssh/authorized_keys":
+            ensure => exists,
+            owner => jenkins,
+            require => File['/var/lib/jenkins/.ssh/']
+    }
+
+    file_line {
+        "jenkins-publish-authorized_key":
+            line => $jenkins_authorized_key
+            path => '/var/lib/jenkins/authorized_keys'
+            require => File['/var/lib/jenkins/authorized_keys']
+            
+    }
 }
