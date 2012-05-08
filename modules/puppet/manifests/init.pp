f218e005c7551b70bf870585cfba5fb6b3e18885
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-08 21:18:38 +0200

    removing noop on Package['puppet']
    I had to patch puppet.
    Pull Request is here: https://github.com/puppetlabs/puppet/pull/740
    The ebuild we now use is here:
    https://github.com/purplehazech/rabe-portage-overlay/tree/master/app-admin/puppet
    
    That's all

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -4,9 +4,6 @@ class puppet {
     package {
         "puppet":
             ensure => installed,
-            # i had to patch /usr/lib64/ruby/site_ruby/1.8/puppet/provider/package/portage.rb
-            # to make it support a different EIX_CACHEFILE
-            noop => true,
 	    before => Service['puppet'];
 
     }
