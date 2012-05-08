04a8637e0cb53bf52a48d64a71b6e9b3eed32c39
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-08 20:32:24 +0200

    fixing puppet not starting issue
    seems like i need to start expressing my relationships differently
    for some core services, just because a package needs nooping it
    should not be disabling puppet to run itself.

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -6,7 +6,9 @@ class puppet {
             ensure => installed,
             # i had to patch /usr/lib64/ruby/site_ruby/1.8/puppet/provider/package/portage.rb
             # to make it support a different EIX_CACHEFILE
-            noop => true;
+            noop => true,
+	    before => Service['puppet'];
+
     }
 
     file { "/etc/puppet/puppet.conf":
@@ -16,7 +18,6 @@ class puppet {
     service { "puppet":
         ensure => running,
 	require => [
-	    Package["puppet"],
 	    File["/etc/puppet/puppet.conf"]
 	]
     }
