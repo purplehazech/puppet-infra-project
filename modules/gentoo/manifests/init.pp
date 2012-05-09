4aa55b5f53324106c8bc0c19398afa48ae02474b
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-09 21:56:45 +0200

    storing all portage world files in a manifest
    i can analyse this later on to check what need to get added to the
    puppet profile of new machines i plan on integrating...

--- modules/gentoo/manifests/init.pp
+++ modules/gentoo/manifests/init.pp
@@ -1,2 +1,11 @@
 class gentoo {
+
+    filebucket { 'gentoo':
+        server => "$puppet_server",
+        path => false; 
+    }
+
+    file { '/var/lib/portage/world':
+        backup => 'gentoo';
+    }
 }
