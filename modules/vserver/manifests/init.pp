d8aca2d55cbeaae5d16ef5f4bf4b739557b6d404
Lucas Bickel <hairmare@purplehaze.ch>
2012-05-25 16:39:55 +0200

    update profile on all next gen boxen

--- modules/vserver/manifests/init.pp
+++ modules/vserver/manifests/init.pp
@@ -3,4 +3,8 @@ class vserver {
 
     require ::puppet
 
+    file {
+        '/etc/make.profile':
+            ensure => '/var/lib/layman/rabe-portage-overlay/profiles/default/linux/amd64/2012.0/rabe.ch/vserver'
+    }
 }
