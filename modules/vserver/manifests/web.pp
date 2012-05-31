773096eb0fb80bcbd9decb5c6ceaf5f833999387
Lucas S Bickel <hairmare@purplehaze.ch>
2012-05-31 04:06:22 +0200

    only minmal ldap for revproxy

--- modules/vserver/manifests/web.pp
+++ modules/vserver/manifests/web.pp
@@ -25,6 +25,10 @@ class vserver::web {
             content => "www-servers/apache proxy proxy_balancer";
         "/etc/portage/package.use/10_apache_ldap":
             content => "www-servers/apache ldap";
+        "/etc/portage/package.use/10_apache_ldap_apr":
+            content => "dev-libs/apr-util ldap";
+        "/etc/portage/package.use/10_apache_ldap_minimal":
+            content => "net-nds/openldap minimal";
         "/etc/conf.d/apache2":
             content => 'APACHE2_OPTS="-D INFO -D SSL -D LANGUAGE -D LDAP -D PROXY -D PROXY_HTML -D AUTHNZ_EXTERNAL"'
     }
