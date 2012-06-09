659cc72a8008b0feb6a6d965b7981b4b3e74711a
Lucas Bickel <hairmare@purplehaze.ch>
2012-06-09 19:18:40 +0200

    windows config for zabbix

--- modules/zabbix/manifests/init.pp
+++ modules/zabbix/manifests/init.pp
@@ -4,19 +4,32 @@ class zabbix {
         ensure => installed
     }
 
-    file { 
-        '/etc/zabbix/zabbix_agentd.conf':
-            content => template("zabbix/zabbix_agentd.conf.erb");
-        '/etc/zabbix/userparameter.d':
-            ensure => directory;
+    file {
+        case $::operatingsystem {
+            gentoo: {
+                '/etc/zabbix/zabbix_agentd.conf':
+                   content => template("zabbix/zabbix_agentd.conf.erb");
+                '/etc/zabbix/userparameter.d':
+                   ensure => directory;
+            }
+            windows: {
+                'C:\zabbix_agentd.conf':
+                   content => template("zabbix/zabbix_agentd.win.conf.erb");
+            }
     }
 
-    service { "zabbix-agentd":
-        ensure => running,
-        require => [
-            Package['zabbix'],
-            File['/etc/zabbix/zabbix_agentd.conf']
-        ]
+    case $::operatingsystem {
+        windows: {
+        },
+        default: {
+            service { "zabbix-agentd":
+                ensure => running,
+                    require => [
+                    Package['zabbix'],
+                    File['/etc/zabbix/zabbix_agentd.conf']
+                ]
+            }
+        }
     }
 
 
