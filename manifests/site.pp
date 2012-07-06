f91015fe4e9fd5bab55141caef44fac9137a16c6
Lucas Bickel <hairmare@purplehaze.ch>
2012-07-06 14:48:07 +0200

    adding dox

---
+++ manifests/site.pp
@@ -0,0 +1,3 @@
+# This Puppet site.pp file is empty.
+#
+# We use an external node classifer (ENC) with this setup.
--- modules/infra/manifests/init.pp
+++ modules/infra/manifests/init.pp
@@ -1,3 +1,10 @@
+# Class: infra
+#
+# This class contains common things we require on all machines that are
+# under Puppet control.
+#
+# It gets added to all nodes by the external node classifier (ENC)
+# 
 class infra {
 
     require zabbix
