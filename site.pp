9168f6ebe517d897557e356e7430fb2d18423f9e
Lucas Bickel <hairmare@purplehaze.ch>
2012-10-31 23:37:51 +0100

    fix modules bs

--- site.pp
+++ site.pp
@@ -1,11 +1,9 @@
 # Module: __site__
 #
 #
-module __site__ {
-    import "modules/*/manifests/*.pp"
+import "modules/*/manifests/*.pp"
 
-    schedule { 'daily':
+schedule { 'daily':
         period => daily,
         repeat => 1;
-    }
 }
