00571d7696f00608cf16832004b3b74377c00dc1
Lucas Bickel <hairmare@purplehaze.ch>
2012-10-31 19:36:12 +0100

    update eixrc to not use a central cachefile
    central cachefiles do not work. I hadn't even though about the whole
    compat if i ever have multiple eix version and have no realized that
    the idea was just plain stupid...

--- modules/eix/manifests/init.pp
+++ modules/eix/manifests/init.pp
@@ -1,3 +1,23 @@
+# Define: eix
+#
+# install and manage eix
+#
+# This module is a bit silly since eix is already a dep for puppet.
+# It used to take care of managing /etc/eixrc and I need it to 
+# righten my silly idea of keeping a central eix-cache. Who knows
+# maybe one-day i'll find a use for doing stuff to eixrc :)
+# 
+#
+# Parameters:
+# - ensure
+#
+# Actions:
+# * Install eix package and manage eixrc
+#
+# Sample Usage:
+# 
+#      eix{ 'install':; }
+#
 define eix (
     $ensure = installed
 ) {
@@ -8,7 +28,7 @@ define eix (
 
     file {
         "/var/cache/eix":
-            ensure => directory;
+            ensure => file;
         "/etc/eixrc":
             content => template("eix/eixrc.erb")
     }
