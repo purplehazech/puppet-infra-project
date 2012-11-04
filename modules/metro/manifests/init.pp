5b782f9a6b62d15c7c4873fa7c7940d67fc116d9
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-04 17:07:20 +0100

    merge?

--- modules/metro/manifests/init.pp
+++ modules/metro/manifests/init.pp
@@ -1,38 +1,41 @@
 # Class: metro
 #
 # Class for deploying metro to a node from git (as recommended in metros dox)
-# 
+#
 
 class metro {
+  class { 'ccache': ; }
 
-    ccache { 'ccache': ; }
+  git::wc { "/var/lib/metro/":
+    ensure => latest,
+    repo   => "https://github.com/purplehazech/metro.git",
+  }
 
-    git::wc {
-        "/var/lib/metro/":
-            ensure => latest,
-            repo   => "https://github.com/purplehazech/metro.git",
-    }
+  file {
+    "/home/mirror/":
+      ensure => directory;
 
-    file {
-	"/home/mirror/":
-		ensure => directory;
-	"/home/mirror/linux/":
-		ensure => directory;
-	"/home/mirror/linux/rabe-dev/x86-32bit/i686":
-		ensure => directory;
-	"/var/lib/jenkins/workspace/rabe-metro-desktop":
-		ensure => link,
-		target => "/var/lib/metro";
-    }
+    "/home/mirror/linux/":
+      ensure => directory;
 
-    class { 'sudo':; }
+    "/home/mirror/linux/rabe-dev/x86-32bit/i686":
+      ensure => directory;
 
-    sudo::conf {
-        'jenkins-alias':
-            content => "Cmnd_Alias JENKINS = /var/lib/metro/scripts/ezbuild.sh rabe-* *\n";
-        'jenkins-user':
-            content => 'jenkins ALL = NOPASSWD: JENKINS';
-    }
+    "/var/lib/jenkins/workspace/rabe-metro-desktop":
+      ensure => link,
+      target => "/var/lib/metro";
+  }
 
+  class { 'sudo':
+  ;
+  }
+
+  sudo::conf {
+    'jenkins-alias':
+      content => "Cmnd_Alias JENKINS = /var/lib/metro/scripts/ezbuild.sh rabe-* *\n";
+
+    'jenkins-user':
+      content => 'jenkins ALL = NOPASSWD: JENKINS';
+  }
 
 }
