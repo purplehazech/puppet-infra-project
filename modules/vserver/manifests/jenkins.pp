6c4ee67b1ace9f914a0db1ed604edeae2a232594
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-04 16:38:38 +0100

    Update modules/vserver/manifests/jenkins.pp
    comment out broken jenkins::plugin

--- modules/vserver/manifests/jenkins.pp
+++ modules/vserver/manifests/jenkins.pp
@@ -1,18 +1,17 @@
-
 class vserver::jenkins {
 
     include ant
     include sudo
 
     class jenkins {
-      jenkins::plugin {
-        analysis-core: ;
-        analysis-collector: ;
-        github: ;
-        puppet: ;
-        mantis: ;
-        php: ;
-      }
+     # jenkins::plugin {
+     #   analysis-core: ;
+     #   analysis-collector: ;
+     #   github: ;
+     #   puppet: ;
+     #   mantis: ;
+     #   php: ;
+     # }
     }
 
 }
