be5f0b38252656868f9d7cfe79e8f8b470bc6889
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-21 22:30:28 +0100

    more fixes

--- modules/zabbix/manifests/params.pp
+++ modules/zabbix/manifests/params.pp
@@ -13,8 +13,8 @@ class zabbix::params {
       $zabbix_supports_userparameters = true
       $zabbix_agentd_install          = true
       $zabbix_agentd_package_name     = 'zabbix-agent'
-      $zabbix_agentd_service_name     = 'zabbix-agentd'
-      $zabbix_agentd_conf_file        = '/etc/zabbix/zabbix_agentd.conf'
+      $zabbix_agentd_service_name     = 'zabbix-agent'
+      $zabbix_agentd_conf_file        = '/etc/zabbix/zabbix_agent.conf'
       $zabbix_agentd_conf_template    = 'zabbix_agentd.conf.erb'
     }
     default : {
