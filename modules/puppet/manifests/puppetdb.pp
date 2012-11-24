b856bdc00d98944de5e83988505a3d56cf9cc42d
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-24 17:25:03 +0100

    basics for puppetdb

---
+++ modules/puppet/manifests/puppetdb.pp
@@ -0,0 +1,18 @@
+# == Class: puppet::puppetdb
+#
+# install and setup a puppetdb instance for each master
+#
+class puppet::puppetdb inherits puppet::params {
+  package { ['puppetdb','puppetdb-terminus']:
+    ensure => installed,
+    before => Service['puppetdb']
+  }
+  service { 'puppetdb':
+    ensure  => running,
+    require => File[$puppet_puppetdb_config_file],
+    notify  => Service['puppetmaster']
+  }
+  file { $puppet_puppetdb_config_file:
+    content => template("puppet/$puppet_puppetdb_config_template")
+  }
+}
