0a3201f4f4559323d187441d6209f6977b8da2ca
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-24 17:35:32 +0100

    added some dox to zabbix module

--- modules/zabbix/manifests/init.pp
+++ modules/zabbix/manifests/init.pp
@@ -1,9 +1,16 @@
 # == Class: zabbix
 #
+# Install and configure zabbix agent on a system. This class is part of our default setup,
+# we install the zabbix agent on every machine and configure them so send info to the
+# server on a regular base. This is simply a classic active zabbix agent setup.
+#
 # === Parameters:
 #
 # [*zabbix_server_ip*]
 #   where to reach the zabbix server instance, may also be a hostname but ips are recommended
+#
+# === Todos:
+# * implement zabbix server with autoprovisioning based on resource collectors
 #
 class zabbix inherits zabbix::params {
   file { $zabbix_agentd_conf_file:
@@ -30,4 +37,5 @@ class zabbix inherits zabbix::params {
     ensure  => running,
     require => $zabbix_service_require
   }
+
 }
