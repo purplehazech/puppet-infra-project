ab1fe0ed4a0aefb06ba0c02449e245600b7e347c
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-25 12:55:26 +0100

    add linting for erb scripts to build process

---
+++ _scripts/erb-lint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Linting ERB files"
+
+for erbfile in $@; do
+        erb -x -T '-' $erbfile | ruby -c
+        if [[ $? != 0 ]]; then
+        		echo "Error in $erbfile"
+                exit 1
+        fi
+done
