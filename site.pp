830bb119df5edd0b18850479917c11abd41e04c1
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-26 21:27:17 +0100

    add some dox, remove wrong global

--- site.pp
+++ site.pp
@@ -1,9 +1,6 @@
-# Module: __site__
+# == Module: __site__
 #
-#
+# Btw in bootstrap this is where we load all the modules
+# this is used to get something to test during automated
+# testing/docbuilding. In production it is not used.
 import "modules/*/manifests/*.pp"
-
-schedule { 'daily':
-        period => daily,
-        repeat => 1;
-}
