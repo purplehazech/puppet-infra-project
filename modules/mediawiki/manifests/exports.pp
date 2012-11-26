988426d9d3f5649de95b738978c31a56d478084f
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-26 20:40:08 +0100

    quite critical bugfix

--- modules/mediawiki/manifests/exports.pp
+++ modules/mediawiki/manifests/exports.pp
@@ -5,7 +5,7 @@ class mediawiki::exports {
     proxy_vhost => 'default_ssl_vhost',
     location    => '/mediawiki_test/',
     url_map     => '/ /mediawiki_test/',
-    dest        => $fqdn,
+    dest        => "http://${fqdn}/",
     tag         => ['intranet', 'extranet']
   }
 }
