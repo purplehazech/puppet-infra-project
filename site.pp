abf3442085e102b5df60c8686d7eb065edfd98c6
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-26 21:27:31 +0100

    stylefix

--- site.pp
+++ site.pp
@@ -3,4 +3,4 @@
 # Btw in bootstrap this is where we load all the modules
 # this is used to get something to test during automated
 # testing/docbuilding. In production it is not used.
-import "modules/*/manifests/*.pp"
+import 'modules/*/manifests/*.pp'
