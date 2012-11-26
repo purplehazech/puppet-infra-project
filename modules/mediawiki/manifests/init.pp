4672e8edcefe68244e3652c0eee46c84b1f7abad
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-26 20:14:24 +0100

    refactor into params file

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -32,36 +32,7 @@
 # [*mediawiki_ldap_proxy_agent_password*]
 #  wgLDAPProxyAgentPassword
 #
-class mediawiki {
-  $mediawiki_raw_html                  = true
-  $mediawiki_allow_copy_uploads        = true
-  $mediawiki_disable_unauthed_edits    = true
-  $mediawiki_enable_uploads            = true
-  $mediawiki_enable_scary_transcluding = true
-  $mediawiki_show_exception_details    = true
-
-  $mediawiki_file_extensions           = ['svg', 'pdf', 'ppt', 'xls', 'dia']
-
-  $mediawiki_ldap_encryption_type      = 'clear'
-
-  $mediawiki_wikieditor                = true
-
-  $mediawiki_socialprofile             = true
-  $mediawiki_disable_anon              = true
-
-  $mediawiki_email_authentication      = false
-  $mediawiki_enotif_user_talk          = true
-  $mediawiki_enotif_watchlist          = true
-  $mediawiki_enable_email              = true
-  $mediawiki_enable_user_email         = false
-
-  $mediawiki_rights_url                = 'http://creativecommons.org/licenses/by-nc-sa/2.5/ch/'
-  $mediawiki_rights_text               = 'Attribution-Noncommercial-Share Alike 2.5 Switzerland License'
-  $mediawiki_rights_icon               = 'https://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png'
-
-  $mediawiki_main_cache_type           = 'CACHE_MEMCACHED'
-  $mediawiki_cached_server             = "${fqdn}:11211"
-
+class mediawiki inherits mediawiki::params {
   file {
     '/etc/portage/package.use/10_php_xmlreader':
       content => 'dev-lang/php xmlreader';
