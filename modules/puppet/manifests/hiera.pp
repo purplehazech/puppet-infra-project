8b891f15fcd35d038b49eef69d030b9aa69107b3
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-27 01:44:28 +0100

    create hiera config file

--- modules/puppet/manifests/hiera.pp
+++ modules/puppet/manifests/hiera.pp
@@ -5,6 +5,8 @@
 # I still need to rollut hiera, this is just a smallish start
 #
 class puppet::hiera inherits puppet::params {
+  file { '/etc/hiera.yaml': content => template('puppet/hiera.yaml.erb') }
+
   package { 'hiera':
     ensure   => absent,
     provider => 'gem'
@@ -16,7 +18,7 @@ class puppet::hiera inherits puppet::params {
   }
 
   # create links for including hiera
-  file { '/usr/share/puppet-infra-project/modules/hiera':
+  file { '/usr/share/puppet/modules/hiera':
     ensure  => absent,
     target  => '/usr/local/lib64/ruby/gems/1.8/gems/hiera-puppet-1.0.0/',
     require => Package['hiera-puppet'],
