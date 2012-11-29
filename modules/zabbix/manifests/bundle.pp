fe9b34978851d72e5c0d20feb07332bd321d389d
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-30 00:31:45 +0100

    finalize first version of install bundle

--- modules/zabbix/manifests/bundle.pp
+++ modules/zabbix/manifests/bundle.pp
@@ -2,14 +2,36 @@
 #
 # installs a zabbixized puppet module on both zabbix server and client
 #
+# Must only be included on the client.
+#
 # === Parameters
 # [*ensure*]
 #   present or absent, present is default
 # [*server_template*]
 #   defaults to "${name}/zabbix-template.xml.erb"
+# [*agent_params*]
+#   hash of user parameters to configure
 #
-class zabbix::bundle ($ensure = 'present', $server_template = undef) inherits zabbix::params {
+# === Example Usage
+#
+class zabbix::bundle (
+  $ensure          = 'present',
+  $server_template = undef,
+  $agent_params    = {
+  }
+) inherits zabbix::params {
   if $server_template == undef {
     $server_template = "${name}/zabbix-template.xml.erb"
   }
+  $ensure_real = $ensure
+
+  @@zabbix::server::template { $name:
+    ensure => $ensure_real
+  }
+
+  zabbix::agent::params { $name:
+    ensure => $ensure_real,
+    params => $agent_params
+  }
+
 }
