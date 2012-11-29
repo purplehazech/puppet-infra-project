78ae76a99e35436332959332d95785a943faddab
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-29 22:13:22 +0100

    fix layman module scoping

--- modules/layman/manifests/overlay.pp
+++ modules/layman/manifests/overlay.pp
@@ -3,15 +3,17 @@
 # add and sync an overlay to layman
 #
 define layman::overlay ($require = [], $schedule = 'daily') {
+  include layman
+
   $layman_dir = getvar('layman::layman_dir')
 
   exec { "layman::overlay-add-repo-${name}":
     command => "/usr/bin/layman -a ${name}",
     creates => "${layman_dir}/${name}",
     require => $require
-  } -> exec { "layman::overlay-sync-repo-${name}": 
-    command => "/usr/bin/layman -s ${name}", 
+  } -> exec { "layman::overlay-sync-repo-${name}":
+    command  => "/usr/bin/layman -s ${name}",
     schedule => $schedule
   }
-}
 
+}
--- modules/layman/manifests/overlay/rabe_portage_overlay.pp
+++ modules/layman/manifests/overlay/rabe_portage_overlay.pp
@@ -1,8 +1,6 @@
 # == Class: layman::overlay::rabe_portage_overlay
 #
 class layman::overlay::rabe_portage_overlay {
-  include layman
-
   layman::overlay { 'rabe-portage-overlay':
   }
 
