d741fb1a09c81b98016b36ac71dbdcac25514701
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-30 03:18:29 +0100

    call example for zabbix_template ruby type

--- modules/zabbix/manifests/server/template.pp
+++ modules/zabbix/manifests/server/template.pp
@@ -6,7 +6,11 @@
 # [*ensure*]
 #   present or absent, default is present
 #
-define zabbix::server::template ($ensure = 'present', $template = "${name}/zabbix-template.xml.erb") {
+define zabbix::server::template ($ensure = 'present', $template = "${name}/zabbix-template.json.erb") {
   zabbix_template { $name:
+    content  => template($template),
+    server   => $zabbix::params::zabbix_frontend_url,
+    user     => 'Admin',
+    password => 'zabbix'
   }
 }
