455d20c884d6c3186b8ce593b6c711f75a3ff9b8
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-30 03:18:06 +0100

    proof of concept type for managing zabbix templates

---
+++ modules/zabbix/lib/puppet/provider/zabbix_template/ruby.rb
@@ -0,0 +1,51 @@
+
+Puppet::Type.type(:zabbix_template).provide(:ruby) do
+
+  def exists?
+    load_server()
+    
+    return @server.template.exists({"host" => resource[:name]})
+  end
+
+  def create
+    load_server()
+    
+    hostgroup_templates = @server.hostgroup.get({
+      "filter" => {
+        "name" => resource[:hostgroup]
+      }
+    })
+	@server.template.create({
+	  "host"   => resource[:name], 
+	  "groups" => hostgroup_templates
+	})
+  end
+
+  def destroy
+    load_server()
+    template_hash = @server.template.get({
+      "filter" => {
+        "host" => resource[:name]
+      }
+    })
+    @server.template.delete(template_hash)
+  end
+  
+  private
+  @server
+  
+  def load_server
+    
+    if !@server 
+
+      require "zbxapi"
+      @server = ZabbixAPI.new(resource[:server])
+      if resource[:proxy_host] != nil 
+        @server.set_proxy(resource[:proxy_host] ,resource[:proxy_password])
+      end
+      @server.login(resource[:user],resource[:password])
+      
+    end
+  end
+
+end
---
+++ modules/zabbix/lib/puppet/type/zabbix_template.rb
@@ -0,0 +1,61 @@
+Puppet::Type.newtype(:zabbix_template) do
+
+  desc <<-EOT
+    Install a zabbix template to a server from a given xml.
+
+    Example:
+    
+        zabbix_template{ 'Template_App_Puppet':
+          ensure => $present,
+        }
+    
+  EOT
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'An arbitrary name used as the identity of the resource.'
+  end
+
+  newparam(:content) do
+    desc 'XML Content for the given template'
+  end
+  
+  newparam(:server) do
+    desc 'URL to zabbix frontend'
+  end
+  
+  newparam(:proxy_host) do
+    desc 'proxy host if needed'
+  end
+  
+  newparam(:proxy_password) do
+    desc 'password to the proxy'
+  end
+  
+  newparam(:user) do
+    desc 'zabbix user'
+  end
+  
+  newparam(:password) do
+    desc 'zabbix users password'
+  end
+  
+  newparam(:hostgroup) do
+    desc 'hostgroup to put template into'
+    defaultto "Templates"
+  end
+
+  validate do
+    unless self[:name] and self[:content] 
+      raise(Puppet::Error, "zabbix_template: Both name and content are required attributes")
+    end
+    unless self[:server] and self[:user] and self[:password]
+      raise(Puppet::Error, "zabbix_template: server configuration is required")
+    end
+
+  end
+end
