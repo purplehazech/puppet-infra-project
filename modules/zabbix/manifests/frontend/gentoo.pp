5c37c68c8ae7b881174f671982503a1d8d054d7f
Lucas Bickel <hairmare@purplehaze.ch>
2012-11-30 01:45:33 +0100

    move use flag where it belongs

--- modules/zabbix/manifests/frontend/gentoo.pp
+++ modules/zabbix/manifests/frontend/gentoo.pp
@@ -3,8 +3,12 @@ class zabbix::frontend::gentoo {
     '/etc/portage/package.use/10_zabbix_frontend':
       content => 'net-analyzer/zabbix  ldap mysql server jabber snmp';
 
+    '/etc/portage/package.use/10_php-zabbix_frontend':
+      content => 'dev-lang/php truetype gd bcmath sockets';
+
     '/etc/portage/package.use/10_gd-zabbix_frontend':
       content => 'media-libs/gd png'
+
   }
 
 }
--- modules/zabbix/manifests/server/template.pp
+++ modules/zabbix/manifests/server/template.pp
@@ -7,5 +7,6 @@
 #   present or absent, default is present
 #
 define zabbix::server::template ($ensure = 'present', $template = "${name}/zabbix-template.xml.erb") {
-
+  zabbix_template { $name:
+  }
 }
