99b7f55987921d12665cf7e53c7762bb26483a96
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-01 12:24:35 +0100

    remove puppet before move to own repo

--- modules/puppet/manifests/activerecord.pp
+++
@@ -1,9 +0,0 @@
-# == Class: puppet::activerecord
-#
-# storeconfigs in activerecord
-#
-class puppet::activerecord inherits puppet::params {
-  package { $puppet_storeconfig_activerecord_packages: ensure => present }
-
-}
-
