f9c1b20c0c80d88fe7c1520c5d9d92fa4706d7fe
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-07 18:47:29 +0200

    remove stuff now possible in enc

--- modules/vserver/manifests/init.pp
+++ modules/vserver/manifests/init.pp
@@ -1,8 +1,6 @@
 
 class vserver {
 
-    require ::puppet
-
     file {
         '/etc/make.profile':
             ensure => '/var/lib/infra/layman/rabe-portage-overlay/profiles/default/linux/amd64/2012.0/rabe.ch/vserver'
