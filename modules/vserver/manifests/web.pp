759411c23721fcb23c0ad549e992da9b0aa252e3
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-08 00:19:06 +0200

    removing stuff that when into the ENC

--- modules/vserver/manifests/web.pp
+++ modules/vserver/manifests/web.pp
@@ -23,58 +23,6 @@
 # These all get passed as global parameters since i am still using the ldap enc
 #
 class vserver::web {
-  class { 'apache': serveradmin => $::http_serveradmin }
-
-  apache::vhost::proxy {
-    'default_vhost':
-      port       => 80,
-      dest       => $::proxy_fallback,
-      servername => 'intranet.rabe.ch';
-
-    'default_ssl_vhost':
-      port       => 443,
-      ssl        => true,
-      ssl_cert   => 'intranet.rabe.ch.crt.pem',
-      ssl_key    => 'intranet.rabe.ch.key.pem',
-      ssl_ca     => 'intermediate.rapidssl.com.ca.pem',
-      dest       => $::proxy_fallback,
-      servername => 'intranet.rabe.ch';
-  }
-
-  if $::proxy_mantisbt != undef {
-    apache::vhost::include::proxy { 'mantis_default_ssl_reverseproxy':
-      proxy_vhost => 'default_ssl_vhost',
-      location    => '/mantisbt/',
-      dest        => $::proxy_mantisbt;
-    }
-  }
-
-  if $::proxy_webdav != undef {
-    apache::vhost::include::proxy { 'webdav_default_ssl_reverseproxy':
-      proxy_vhost => 'default_ssl_vhost',
-      location    => '/webdav/',
-      url_map     => '/ /webdav/',
-      dest        => $::proxy_webdav;
-    }
-  }
-
-  if $::proxy_jenkins != undef {
-    apache::vhost::include::proxy { 'jenkins_default_ssl_reverseproxy':
-      proxy_vhost => 'default_ssl_vhost',
-      location    => '/jenkins',
-      url_map     => '/ /jenkins',
-      dest        => $::proxy_jenkins;
-    }
-  }
-
-  if $::proxy_mediawiki != undef {
-    apache::vhost::include::proxy { 'mediawiki_default_ssl_reverseproxy':
-      proxy_vhost => 'default_ssl_vhost',
-      location    => '/mediawiki/',
-      url_map     => '/ /mediawiki/',
-      dest        => $::proxy_mediawiki;
-    }
-  }
 
   Apache::Vhost::Include::Proxy <<| tag == 'extranet' |>>
 
